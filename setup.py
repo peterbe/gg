72a3fd0896fac3e9da86e6d6902aac273c1db570
Peter Bengtsson <mail@peterbe.com>
2016-04-21 08:11:50 -0400

    description and author

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='gg',
     version='0.0.1',
+    author='Peter Bengtsson',
+    author_email='mail@peterbe.com',
+    url='https://github.com/peterbe/gg',
+    description='Git and GitHub command line swiss army knife',
     # py_modules=['gg'],
     install_requires=[
         'click',
