a900ce17ab2f945f9c37db854e93dfa70cc16b6b
Peter Bengtsson <mail@peterbe.com>
2018-05-09 15:44:30 -0400

    tox finally works

--- setup.py
+++ setup.py
@@ -13,6 +13,14 @@ setup(
     description='Git and GitHub for the productivity addicted',
     long_description=open(path.join(_here, 'README.rst')).read(),
     license='MIT',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: MIT License",
+    ],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -28,4 +36,5 @@ setup(
     """,
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-mock'],
+    keywords="git click github bugzilla",
 )
