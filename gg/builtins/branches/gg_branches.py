47f97bcac36f81f9986c2d6f07649fb7f905d085
Peter Bengtsson <mail@peterbe.com>
2018-05-16 15:38:02 -0400

    black stuff

--- gg/builtins/branches/gg_branches.py
+++ gg/builtins/branches/gg_branches.py
@@ -4,6 +4,7 @@ import git
 import click
 
 from gg.utils import error_out, get_repo, info_out, success_out
+
 # from gg.state import read
 from gg.main import cli, pass_config
 
@@ -11,7 +12,7 @@ from gg.main import cli, pass_config
 @cli.command()
 @click.argument("searchstring", default="")
 @pass_config
-def branches(config, searchstring=''):
+def branches(config, searchstring=""):
     """List all branches. And if exactly 1 found, offer to check it out."""
     try:
         repo = get_repo()
@@ -30,10 +31,10 @@ def branches(config, searchstring=''):
                 error_out("You're already on '{}'".format(branches_[0].name))
             branch_name = branches_[0].name
             if len(branch_name) > 50:
-                branch_name = branch_name[:47] + '…'
+                branch_name = branch_name[:47] + "…"
             check_it_out = input(
                 "Check out '{}'? [Y/n] ".format(branch_name)
-            ).lower().strip() != 'n'
+            ).lower().strip() != "n"
             if check_it_out:
                 branches_[0].checkout()
                 # raise Exception('CHECK OUT ' +branch_name)
@@ -57,11 +58,8 @@ def find(repo, searchstring):
 def get_merged_branches(repo):
     # XXX I wish I could dedude from a git.refs.head.Head object if it was
     # merged. Then I wouldn't have to do this string splitting crap.
-    output = repo.git.branch('--merged')
-    return [
-        x.split()[-1] for x in output.splitlines()
-        if x.strip()
-    ]
+    output = repo.git.branch("--merged")
+    return [x.split()[-1] for x in output.splitlines() if x.strip()]
 
 
 # def branch_info(head):
@@ -73,25 +71,16 @@ def print_list(heads, merged_names):
         try:
             most_recent = head.log()[-1]
             return {
-                'head': head,
-                'info': {
-                    'date': datetime.datetime.utcfromtimestamp(most_recent.time[0]),
-                    'message': most_recent.message,
-                    'oldhexsha': most_recent.oldhexsha,
-                }
-            }
-        except IndexError:
-            return {
-                'head': head,
-                'info': {
-                    'date': datetime.datetime.utcnow(),
+                "head": head,
+                "info": {
+                    "date": datetime.datetime.utcfromtimestamp(most_recent.time[0]),
+                    "message": most_recent.message,
+                    "oldhexsha": most_recent.oldhexsha,
                 },
             }
-            info = {
-                'date': None,
-                'message': None,
-                'oldhexsha': None,
-            }
+        except IndexError:
+            return {"head": head, "info": {"date": datetime.datetime.utcnow()}}
+            info = {"date": None, "message": None, "oldhexsha": None}
         return info
 
     def format_age(dt):
@@ -100,20 +89,19 @@ def print_list(heads, merged_names):
 
     def format_msg(message):
         if len(message) > 80:
-            return message[:76] + '…'
+            return message[:76] + "…"
         return message
 
     wrapped = sorted(
-        [wrap(head) for head in heads],
-        key=lambda x: x['info'].get('date')
+        [wrap(head) for head in heads], key=lambda x: x["info"].get("date")
     )
     for each in wrapped:
-        info_out(''.center(80, '-'))
+        info_out("".center(80, "-"))
         success_out(
-            each['head'].name +
-            (each['head'].name in merged_names and ' (MERGED ALREADY)' or ''),
+            each["head"].name
+            + (each["head"].name in merged_names and " (MERGED ALREADY)" or "")
         )
-        info_out('\t' + each['info']['date'].isoformat())
-        info_out('\t' + format_age(each['info']['date']))
-        info_out('\t' + format_msg(each['info'].get('message', '*no commit yet*')))
-        info_out('')
+        info_out("\t" + each["info"]["date"].isoformat())
+        info_out("\t" + format_age(each["info"]["date"]))
+        info_out("\t" + format_msg(each["info"].get("message", "*no commit yet*")))
+        info_out("")
