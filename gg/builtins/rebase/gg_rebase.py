a6c4a4ab456d124183bd940a0e5e83df22c97224
Peter Bengtsson <mail@peterbe.com>
2018-08-24 10:38:55 -0400

    better rebase

--- gg/builtins/rebase/gg_rebase.py
+++ gg/builtins/rebase/gg_rebase.py
@@ -1,4 +1,4 @@
-from gg.utils import error_out, success_out
+from gg.utils import error_out, success_out, info_out
 from gg.state import read
 from gg.main import cli, pass_config
 
@@ -12,6 +12,7 @@ def rebase(config):
     active_branch = repo.active_branch
     if active_branch.name == "master":
         error_out("You're already on the master branch.")
+    active_branch_name = active_branch.name
 
     if repo.is_dirty():
         error_out(
@@ -30,5 +31,11 @@ def rebase(config):
     if not upstream_remote:
         error_out("No remote called {!r} found".format(origin_name))
 
-    repo.git.rebase("{}/master".format(origin_name))
+    repo.heads.master.checkout()
+    repo.remotes[origin_name].pull()
+
+    repo.heads[active_branch_name].checkout()
+
+    print(repo.git.rebase("master"))
     success_out("Rebased against {}/master".format(origin_name))
+    info_out("If you wanto start interactive rebase run:\n\n\tgit rebase -i master\n")
